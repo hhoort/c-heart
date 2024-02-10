b63a0f271deb0e453a71765d538f5bbc94446e83
unknown <hhoor@gmail.com>
2024-02-10 04:56:42 -0800

    first commit

---
+++ Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Red; // Set text color to red
+
+
+        Console.WriteLine("       ♥♥♥♥    ♥♥♥♥♥       ");
+        Console.WriteLine("      ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥        ");
+        Console.WriteLine("    ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥        ");
+        Console.WriteLine("   ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥      ");
+        Console.WriteLine("  ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥   ");
+        Console.WriteLine(" ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥ ");
+        Console.WriteLine(" ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥ ");
+        Console.WriteLine(" ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥  ");
+        Console.WriteLine("   ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥  ");
+        Console.WriteLine("    ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥    ");
+        Console.WriteLine("     ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥    ");
+        Console.WriteLine("       ♥♥♥♥♥♥♥♥♥♥♥♥♥♥       ");
+        Console.WriteLine("         ♥♥♥♥♥♥♥♥♥♥♥         ");
+        Console.WriteLine("           ♥♥♥♥♥♥♥♥           ");
+        Console.WriteLine("            ♥♥♥♥♥♥          ");
+        Console.WriteLine("             ♥♥♥♥       ");
+        Console.WriteLine("              ♥♥♥           ");
+        Console.WriteLine("               ♥♥           ");
+        Console.ResetColor(); // Reset text color to default
+    }
+}
